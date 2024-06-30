0334a4e6595f286cfbfc02aa21163e1818d1f31a
gmstraw0454 <gmstraw0454@gmail.com>
2024-07-01 00:40:23 +0900

    commit

---
+++ Chapter 3/code/3_23.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+int __fastcall sum(int a, int b, int c) {
+    return a + b + c;
+}
+
+int main()
+{
+    int s;
+
+    s = sum(1, 2, 3);
+    printf("% d\n", s);
+
+    return 0;
+}
+
