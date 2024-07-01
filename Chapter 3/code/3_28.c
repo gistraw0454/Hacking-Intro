fdbf7fb09c59554cc9fa5049bdbb05d4702aa30e
gmstraw0454 <gmstraw0454@gmail.com>
2024-07-01 20:01:36 +0900

    commit

---
+++ Chapter 3/code/3_28.c
@@ -0,0 +1,8 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    printf("prologue & epilogue\n");
+
+    return 0;
+}
---
+++ Chapter 3/code/3_32.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+int inc(int a) {
+	a = a + 1;
+	return a;
+}
+
+int main() {
+	int s,ret;
+
+	s = 2;
+	ret = inc(s);
+
+	printf("%d, %d\n",s,ret);
+	
+	return 0;
+
+}
